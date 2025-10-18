8d1eda3a7340d60d6e5923eafd46aeced8964ffc
Patrick Schneider <urulu@student.kit.edu>
2025-10-18 19:16:06 +0200

    Converts ShakeListener to Kotlin.

--- app/src/main/java/org/secuso/privacyfriendlydicer/sensors/ShakeListener.kt
+++ app/src/main/java/org/secuso/privacyfriendlydicer/sensors/ShakeListener.kt
@@ -1,65 +1,59 @@
-package org.secuso.privacyfriendlydicer.sensors;
+package org.secuso.privacyfriendlydicer.sensors
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import kotlin.math.sqrt
 
-public class ShakeListener implements SensorEventListener {
+class ShakeListener(private val shakeThreshold: () -> Float) : SensorEventListener {
+    var onShakeListener: OnShakeListener? = null
+    private var mShakeTimestamp: Long = 0
+    private var mShakeCount = 0
 
-    //For shaking sensitivity
-    private static final float SHAKE_THRESHOLD_GRAVITY = 1.8F;
-    private static final int SHAKE_SLOP_TIME_MS = 500;
-    private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
-
-    private OnShakeListener mListener;
-    private long mShakeTimestamp;
-    private int mShakeCount;
-
-    public void setOnShakeListener(OnShakeListener listener) {
-        this.mListener = listener;
+    fun interface OnShakeListener {
+        fun onShake(count: Int)
     }
 
-    public interface OnShakeListener {
-        public void onShake(int count);
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    override fun onSensorChanged(event: SensorEvent) {
+        if (onShakeListener != null) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
 
-    }
+            val gX = x / SensorManager.GRAVITY_EARTH
+            val gY = y / SensorManager.GRAVITY_EARTH
+            val gZ = z / SensorManager.GRAVITY_EARTH
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
+            val gForce = sqrt((gX * gX + gY * gY + gZ * gZ).toDouble()).toFloat()
 
-        if (mListener != null) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-
-            float gX = x / SensorManager.GRAVITY_EARTH;
-            float gY = y / SensorManager.GRAVITY_EARTH;
-            float gZ = z / SensorManager.GRAVITY_EARTH;
-
-            float gForce = (float)Math.sqrt(gX * gX + gY * gY + gZ * gZ);
-
-            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
-                final long now = System.currentTimeMillis();
+            if (gForce > shakeThreshold()) {
+                val now = System.currentTimeMillis()
                 // ignore shake events too close to each other (500ms)
                 if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
-                    return;
+                    return
                 }
 
                 // reset the shake count after 3 seconds of no shakes
                 if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
-                    mShakeCount = 0;
+                    mShakeCount = 0
                 }
 
-                mShakeTimestamp = now;
-                mShakeCount++;
+                mShakeTimestamp = now
+                mShakeCount++
 
-                mListener.onShake(mShakeCount);
+                onShakeListener!!.onShake(mShakeCount)
             }
         }
+    }
+
+    companion object {
+        //For shaking sensitivity
+        private const val SHAKE_THRESHOLD_GRAVITY = 1.8f
+        private const val SHAKE_SLOP_TIME_MS = 500
+        private const val SHAKE_COUNT_RESET_TIME_MS = 3000
     }
 }
