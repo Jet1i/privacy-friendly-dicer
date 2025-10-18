83db75fe3b075ee53f9e44a57314fdedca9c82ea
Patrick Schneider <urulu@student.kit.edu>
2025-10-18 18:00:52 +0200

    Updates MainActivity and refactors layout files.

--- app/src/main/java/org/secuso/privacyfriendlydicer/ui/MainActivity.kt
+++ app/src/main/java/org/secuso/privacyfriendlydicer/ui/MainActivity.kt
@@ -1,65 +1,55 @@
 package org.secuso.privacyfriendlydicer.ui
 
-import android.content.Intent
-import android.content.SharedPreferences
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Typeface
 import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Vibrator
-import android.preference.PreferenceManager
-import android.view.MenuItem
-import android.view.View
-import android.view.animation.AlphaAnimation
-import android.view.animation.Animation
-import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
-import androidx.annotation.DrawableRes
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
-import com.google.android.material.navigation.NavigationView
+import org.secuso.pfacore.model.DrawerElement
+import org.secuso.pfacore.model.DrawerMenu
+import org.secuso.pfacore.ui.activities.DrawerActivity
+import org.secuso.privacyfriendlydicer.PFApplicationData
 import org.secuso.privacyfriendlydicer.R
 import org.secuso.privacyfriendlydicer.databinding.ActivityMainBinding
-import org.secuso.privacyfriendlydicer.databinding.ContentMainBinding
 import org.secuso.privacyfriendlydicer.sensors.ShakeListener
-import org.secuso.privacyfriendlydicer.sensors.ShakeListener.OnShakeListener
 import java.util.Locale
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : DrawerActivity() {
     private val dicerViewModel by lazy { ViewModelProvider(this).get<DicerViewModel>(DicerViewModel::class.java) }
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private val contentMainBinding by lazy { ContentMainBinding.bind(binding.root) }
-
     private val adapter: DiceAdapter by lazy { DiceAdapter(dicerViewModel.dicerLiveData.value ?: IntArray(0), layoutInflater) }
-    private var shakingEnabled = false
-    private var vibrationEnabled = false
-    private val sharedPreferences by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
+    private val shakingEnabled by lazy { PFApplicationData.instance(this).rollByShaking }
+    private val vibrationEnabled by lazy { PFApplicationData.instance(this).enableVibration }
 
     // for Shaking
     private val sensorManager: SensorManager by lazy { getSystemService(SENSOR_SERVICE) as SensorManager }
     private val accelerometer: Sensor? by lazy { sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) }
     private val shakeListener = ShakeListener()
 
+    override fun drawer() = DrawerMenu.build {
+        name = getString(R.string.app_name)
+        icon = R.mipmap.ic_drawer
+        section {
+            activity {
+                name = getString(R.string.activity_main_title)
+                icon = R.drawable.ic_menu_dice
+                clazz = MainActivity::class.java
+            }
+        }
+        defaultDrawerSection(this)
+    }
+
+    override fun isActiveDrawerElement(element: DrawerElement) = element.icon == R.drawable.ic_menu_dice
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         
-        contentMainBinding.dices.adapter = adapter
-        contentMainBinding.dices.layoutManager = object : GridLayoutManager(this, 5, VERTICAL, false) {
-            override fun canScrollHorizontally() = false
-            override fun canScrollVertically() = false
-        }
+        binding.dices.adapter = adapter
 
         initResources()
 
@@ -68,7 +58,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 adapter.dices = dice
                 displaySum()
 
-                if (vibrationEnabled) {
+                if (vibrationEnabled.value) {
                     val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
                     vibrator.vibrate(50)
                 }
@@ -77,27 +67,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         dicerViewModel.diceNumberLiveData.observe(this, object : Observer<Int> {
             override fun onChanged(number: Int) {
-                contentMainBinding.chooseDiceNumber.text = String.format(Locale.ENGLISH, "%d", number)
+                binding.chooseDiceNumber.text = String.format(Locale.ENGLISH, "%d", number)
             }
         })
 
         dicerViewModel.faceNumberLiveData.observe(this, object : Observer<Int> {
             override fun onChanged(number: Int) {
-                contentMainBinding.chooseFaceNumber.text = String.format(Locale.ENGLISH, "%d", number)
+                binding.chooseFaceNumber.text = String.format(Locale.ENGLISH, "%d", number)
             }
         })
     }
 
     private fun initResources() {
-        setSupportActionBar(binding.toolbar)
-
-        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        binding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        binding.navView.setNavigationItemSelectedListener(this)
-
-        contentMainBinding.seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+        binding.seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 dicerViewModel.setDiceNumber(progress + 1)
             }
@@ -109,7 +91,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
         })
 
-        contentMainBinding.seekBarFace.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+        binding.seekBarFace.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 dicerViewModel.setFaceNumber(progress + 1)
             }
@@ -122,34 +104,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         })
 
         //Button
-        contentMainBinding.rollButton.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                rollDice()
-            }
-        })
+        binding.rollButton.setOnClickListener { rollDice() }
 
         //Shaking
-        shakeListener.setOnShakeListener(object : OnShakeListener {
-            override fun onShake(count: Int) {
-                if (shakingEnabled) {
-                    rollDice()
-                }
+        shakeListener.setOnShakeListener {
+            if (shakingEnabled.value) {
+                rollDice()
             }
-        })
+        }
     }
 
     fun rollDice() {
-        applySettings()
         dicerViewModel.rollDice()
     }
 
     private fun displaySum() {
-        contentMainBinding.sumTextView.text = getString(R.string.main_dice_sum, adapter.dices.sum().toString())
-    }
-
-    fun applySettings() {
-        shakingEnabled = sharedPreferences.getBoolean("enable_shaking", true)
-        vibrationEnabled = sharedPreferences.getBoolean("enable_vibration", true)
+        binding.sumTextView.text = getString(R.string.main_dice_sum, adapter.dices.sum().toString())
     }
 
     public override fun onResume() {
@@ -158,39 +128,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             shakeListener, accelerometer,
             SensorManager.SENSOR_DELAY_UI
         )
-
-        applySettings()
     }
 
     public override fun onPause() {
         sensorManager.unregisterListener(shakeListener)
         super.onPause()
-    }
-
-    override fun onBackPressed() {
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-
-    /**
-     * Handle navigation view item clicks here.
-     */
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-
-        val intent = when (item.itemId) {
-            R.id.nav_about -> Intent(this, AboutActivity::class.java)
-            R.id.nav_help -> Intent(this, HelpActivity::class.java)
-            R.id.nav_settimgs -> Intent(this, SettingsActivity::class.java)
-            R.id.nav_tutorial -> Intent(this, TutorialActivity::class.java)
-            else -> return false
-        }
-
-        startActivity(intent)
-        binding.drawerLayout.closeDrawer(GravityCompat.START)
-        return true
     }
 }
